3d0a0a6d5d19e1cac736684cbf254db358094e03
pushkar45756 <pushkar.srivastava@infostride.com>
2022-08-06 01:09:25 +0530

    program corrcted

--- programs/perfect_square.py
+++ programs/perfect_square.py
@@ -3,16 +3,20 @@
 def isPerfectSquare(number):
     isPS=False
     sqrt=None
-    for i in range(1,int(number/2)):
+    
+    for i in range(1,int(number/2)+2):
         if number==i*i:
             isPS = True
             sqrt = i
             break
+        elif i*i>number:
+            break
+
     return isPS,sqrt
 
 
 
 
-number = 9000000
+number = 10
 check,sqrt = isPerfectSquare(number)
 print("Is {} a perfect square ? {} and it's square root is {}".format(number,check,sqrt))
